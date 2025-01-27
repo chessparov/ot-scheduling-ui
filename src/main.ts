68b66d8f8bc45673d8d9037cde7df69a046cb7c8
Cristian <c.agnetti@studenti.unipi.it>
2025-01-27 13:51:28 +0100

    Installed vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,28 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createVuestic } from "vuestic-ui";
 import VueApexCharts from "vue3-apexcharts";
+import * as components from 'vuetify/components'
 
 
 import vuesticGlobalConfig from "./services/vuestic-ui/global-config";
 import stores from "./stores";
 import router from "./router";
 import axios from "axios";
+import {createVuetify} from "vuetify";
+
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:8000";
 
 const app = createApp(App);
 
+const vuetify = createVuetify({
+    components,
+})
 app.use(stores);
 app.use(router);
 app.use(VueApexCharts);
 app.use(createVuestic({ config: vuesticGlobalConfig }));
+app.use(vuetify)
 
 app.mount("#app");
