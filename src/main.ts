8b26ce87f79b97ca9a5084c4b87ee80063fa9a29
Cristian <c.agnetti@studenti.unipi.it>
2025-02-03 15:30:54 +0100

    Deleted vuetify from dependencies.

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createVuestic } from "vuestic-ui";
 import VueApexCharts from "vue3-apexcharts";
-import * as components from 'vuetify/components'
-
 
 import vuesticGlobalConfig from "./services/vuestic-ui/global-config";
 import stores from "./stores";
 import router from "./router";
 import axios from "axios";
-import {createVuetify} from "vuetify";
 
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.xsrfCookieName = "csrftoken";
@@ -20,13 +17,9 @@ axios.defaults.baseURL = "http://localhost:8000";
 
 const app = createApp(App);
 
-const vuetify = createVuetify({
-    components,
-})
 app.use(stores);
 app.use(router);
 app.use(VueApexCharts);
 app.use(createVuestic({ config: vuesticGlobalConfig }));
-app.use(vuetify)
 
 app.mount("#app");
